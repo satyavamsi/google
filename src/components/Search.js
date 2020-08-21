14b87597c5aa02bf663d323a5e65425ce8a1cb06
Satya Vamsi <satya.vamsi@alphanodus.com>
2020-08-21 22:07:59 +0530

    added search results page

---
+++ src/components/Search.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
+
+
+import './Search.css'
+
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core';
+
+import { useStateValue } from '../StateProvider'
+import { actionTypes } from '../reducer';
+
+function Search({ hideButtons = false }) {
+
+    const [state, dispatch] = useStateValue();
+
+    const [input, setInput] = useState("");
+    const history = useHistory();
+
+    const search = e => {
+        e.preventDefault();
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        })
+        setInput("");
+        history.push("/search");
+    }
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
+
+    return (
+        <form className="search">
+            <div className="search__input">
+                <SearchIcon className="search__inputIcon" />
+                <input onChange={handleChange} value={input} type="text" />
+                <MicIcon className="search__inputIcon" />
+            </div>
+            <div className="search__buttons">
+                <Button className={hideButtons ? "search_buttonsHidden" : ""} type="submit" onClick={search} variant="outlined">Google Search</Button>
+                <Button className={hideButtons ? "search_buttonsHidden" : ""} variant="outlined">I'm Feeling Lucky</Button>
+            </div>
+
+        </form>
+    )
+}
+
+export default Search;
